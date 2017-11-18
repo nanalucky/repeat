8351379bfa7faf24e0f145e81e53c1006eb165db
nanalucky <nana_lucky@msn.com>
2017-11-19 00:33:06 +0800

    add fiddlercore

---
+++ repeat/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace repeat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            repeatFiddler.doRun();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            repeatFiddler.Init();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            repeatFiddler.doQuit();
+        }
+    }
+}
---
+++ repeat/Http.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Windows.Forms;
+using Fiddler;
+using FiddlerCore;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Reflection;
+
+namespace repeat
+{
+    public class Player
+    {
+    };
+
+    class AllPlayers
+    {
+
+        public void Init()
+        {
+        }
+  
+    };
+
+    public class repeatFiddler
+    {
+        private static void ReadSessions(List<Fiddler.Session> oAllSessions)
+        {
+            Session[] oLoaded = Utilities.ReadSessionArchive(Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+                                                           + Path.DirectorySeparatorChar + "sazsendertest.saz", false);
+
+            if ((oLoaded != null) && (oLoaded.Length > 0))
+            {
+                oAllSessions.AddRange(oLoaded);
+                //WriteCommandResponse("Loaded: " + oLoaded.Length + " sessions.");
+            }
+        }
+
+        public static void doRun()
+        {
+            List<Fiddler.Session> listSession = new List<Fiddler.Session>();
+            ReadSessions(listSession);
+        }
+        
+        
+        public static void Init()
+        {
+            // <-- Personalize for your Application, 64 chars or fewer
+            Fiddler.FiddlerApplication.SetAppDisplayName("RepeatFiddler");
+
+            string sSAZInfo = Assembly.GetAssembly(typeof(Ionic.Zip.ZipFile)).FullName;
+            DNZSAZProvider.fnObtainPwd = () =>
+            {
+                Console.WriteLine("Enter the password (or just hit Enter to cancel):");
+                string sResult = Console.ReadLine();
+                Console.WriteLine();
+                return sResult;
+            };
+
+            FiddlerApplication.oSAZProvider = new DNZSAZProvider();
+
+            Console.WriteLine(String.Format("Starting {0} ({1})...", Fiddler.FiddlerApplication.GetVersionString(), sSAZInfo));
+
+            Fiddler.CONFIG.IgnoreServerCertErrors = false;
+            FiddlerApplication.Prefs.SetBoolPref("fiddler.network.streaming.abortifclientaborts", true);
+            FiddlerCoreStartupFlags oFCSF = FiddlerCoreStartupFlags.Default;
+            int iPort = 8877;
+            Fiddler.FiddlerApplication.Startup(iPort, oFCSF);
+        }
+
+
+        public static void doQuit()
+        {
+            Fiddler.FiddlerApplication.Shutdown();        
+        }
+    };
+     
+}
